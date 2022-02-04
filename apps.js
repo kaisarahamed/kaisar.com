cab4ebb75fd6a4eba5d2ce5a95484a7a6f2560e8
Kaisar ahamed <kaisarahamed323266@gmail.com>
2022-02-04 09:18:22 +0600

    first commit

---
+++ apps.js
@@ -0,0 +1,51 @@
+document.getElementById("home").addEventListener("click", function(){
+    const allBlog = document.getElementById("all-blog");
+    allBlog.style.display = "block";
+    // const sectionFour = document.getElementById("section4");
+    // sectionFour.style.display = "block";  
+})
+document.getElementById("about").addEventListener("click", function(){
+    const allBlog = document.getElementById("all-blog");
+    allBlog.style.display = "none";
+    const sectionFour = document.getElementById("section4");
+    sectionFour.style.display = "block";  
+})
+document.getElementById("profile").addEventListener("click", function(){
+    const allBlog = document.getElementById("section4");
+    allBlog.style.display = "none";
+    const sectionFive = document.getElementById("section5");
+    sectionFive.style.display = "block";  
+})
+document.getElementById("project").addEventListener("click", function(){
+    const allBlog = document.getElementById("section5");
+    allBlog.style.display = "none";
+    const sectionFive = document.getElementById("section6");
+    sectionFive.style.display = "block";  
+})
+document.getElementById("skill").addEventListener("click", function(){
+    const allBlog = document.getElementById("section6");
+    allBlog.style.display = "none";
+    const sectionFive = document.getElementById("section7");
+    sectionFive.style.display = "block";  
+})
+document.getElementById("experiences").addEventListener("click", function(){
+    const allBlog = document.getElementById("section7");
+    allBlog.style.display = "none";
+    const sectionFive = document.getElementById("section8");
+    sectionFive.style.display = "block";  
+})
+document.getElementById("content").addEventListener("click", function(){
+    const allBlog = document.getElementById("section8");
+    allBlog.style.display = "none";
+    const sectionFive = document.getElementById("section9");
+    sectionFive.style.display = "block";  
+})
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
